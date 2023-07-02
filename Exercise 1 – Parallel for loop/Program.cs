baa22ddcf1763eb8f92ef848538cbb17fb6e3ba2
Luke <lukasz.kutynski@microsoft.wsei.edu.pl>
2023-07-02 12:38:52 +0200

    Add project files.

---
+++ Exercise 1 – Parallel for loop/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+The standard(sequential) for loop runs using a single thread - each iteration is executed
+only after the previous one has finished. With parallel for, iterations are run simultaneously,
+using multiple threads – many iterations can be run at the same time, however not in
+order. Using parallel programming can often significantly speed up calculations.
+Links:
+• https://dotnettutorials.net/lesson/parallel-for-method-csharp/
+• https://www.dotnetperls.com/parallel-for
+Creating threads takes time. This is why going parallel sometimes slows the computation.
+It is always recommended to check the computation time - you can use for example the
+built-in classes Stopwatch or DateTime. Stopwatch is more precise. Links:
+• https://stackoverflow.com/questions/10418493/why-was-the-parallel-version-slowerthan-the-sequential-version-in-this-example
+• https://stackoverflow.com/questions/2923283/stopwatch-vs-using-system-datetimenow-for-timing-events
+*/
+
+
+using System;
+using System.Diagnostics; // needed for Stopwatch
+using System.Threading.Tasks; // needed for Parallel.For
+namespace Ex_06_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical processors: " + Environment.ProcessorCount);
+            // Two large arrays
+            int length = 100000; // try also 10 or 100
+            int[] tab1 = new int[length];
+            int[] tab2 = new int[length];
+            ////////////////////////////////////////////////////////
+            // Standard (sequential) for loop
+            Console.WriteLine("Standard for");
+            Stopwatch sw1 = Stopwatch.StartNew(); // start measuring time 1
+            for (int i = 0; i < length; i++)
+            {
+                tab1[i] = SumNumbersUpTo(i);
+            }
+            sw1.Stop(); // finish measuring time 1
+                        ////////////////////////////////////////////////////////
+                        // Parallel for loop
+            Console.WriteLine("Parallel for");
+            Stopwatch sw2 = Stopwatch.StartNew(); // start measuring time 2
+            Parallel.For(0, length, i =>
+            {
+                tab2[i] = SumNumbersUpTo(i);
+            });
+            sw2.Stop(); // finish measuring time 2
+                        ////////////////////////////////////////////////////////
+                        // Results
+            Console.WriteLine("Standard time: " + sw1.Elapsed);
+            Console.WriteLine("Parallel time: " + sw2.Elapsed);
+        }
+        /**********************************************************************/
+        // This method sums all numbers from 0 to n
+        static int SumNumbersUpTo(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+                sum += i;
+            return sum;
+        }
+    }
+}
---
+++ Exercise 5 – Asynchronous computation/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Asynchronous methods don’t block the current thread, so you can execute other
+commands while the asynchronous code runs in the background. This is useful when
+performing long operations, such as accessing large data or computing heavy functions.
+✔ async Task / async Task<...> → denotes a method containing asynchronous code,
+✔ Task.Run → makes a given part of code calculate asynchronously
+✔ await → forces the current method to wait for the asynchronous code to finish,
+while returning the control to the caller (the method outside, so it can go forward),
+✔ Task.Wait() → blocks the execution of the current method until the asynchronous
+task is finished (the method outside cannot go forward).
+Be careful! If you skip or forget to use the await or Task.Wait commands, the method may
+not finish before the program ends, therefore not finishing its job at all! Also, it is preferred
+to use await, rather than Task.Wait, as await does not block the execution.
+Links:
+• https://www.pluralsight.com/guides/understand-control-flow-async-await
+• https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/await
+• https://www.c-sharpcorner.com/article/async-and-await-in-c-sharp/
+• https://stackoverflow.com/questions/13140523/await-vs-task-wait-deadlock
+*/
+
+using System;
+using System.Threading.Tasks; // needed for asynchronous computation
+namespace Ex_06_05
+{
+    class Program
+    {
+        const int printNumber = 500;
+        static async Task Main(string[] args)
+        {
+            Task t = AsyncPrint();
+            StandardPrint(); // this will run even before AsyncPrint finishes
+            await t; // wait with next lines until Task t is finished
+            Console.WriteLine("Done!");
+        }
+        /**********************************************************************/
+        // Asynchronous printing
+        static async Task AsyncPrint()
+        {
+            // for learning purposes, we start with a standard (synchronous) part
+            for (int i = 0; i < printNumber; i++)
+                Console.Write("*");
+            // Task.Run creates an asynchronous part of code
+            // await forces the method to wait for the asynchronous code to finish
+            await Task.Run(() =>
+            {
+                for (int i = 0; i <= printNumber; i++)
+                    Console.Write(i + " ");
+            });
+        }
+        /**********************************************************************/
+        // Standard printing
+        static void StandardPrint()
+        {
+            for (int i = 0; i < printNumber; i++)
+                Console.Write(".");
+        }
+    }
+}
